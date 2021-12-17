a6a0bb6d8c55aa36c2fd55012333b15a685f6191
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2021-12-17 16:37:22 +0100

    Finished crud on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,19 +3,54 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
-    @post = Post.new(title: '1245')
+    @post = Post.new()
   end
 
   def create
     @post = Post.new(post_param)
 
     if @post.save
+      flash.notice = "Post saved"
       redirect_to posts_path
     else
+      flash.alert = "Can't save post"
       render :new
     end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_param)
+      flash.notice = "Post saved"
+      redirect_to posts_path
+    else
+      flash.alert = "Can't save post"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash.notice = "Post destroyed"
+      redirect_to posts_path
+    else
+      flash.alert = "Can't destroy post"
+      redirect_to posts_path
+    end
+  end
+
 
   def post_param
     params.require(:post).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # get 'posts' => 'posts#index'
-  # get 'posts/new' => 'posts#new'
-  # post 'posts' => 'post#create'
-
   resources :posts
 
   # Defines the root path route ("/")
