a278c967f2e49eba6aac555a098262274bfe86a3
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2021-12-17 11:00:33 +0100

    Create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new(title: '1245')
+  end
+
+  def create
+    @post = Post.new(post_param)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def post_param
+    params.require(:post).permit(
+      :title,
+      :text
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'posts' => 'posts#index'
+  # get 'posts' => 'posts#index'
+  # get 'posts/new' => 'posts#new'
+  # post 'posts' => 'post#create'
+
+  resources :posts
 
   # Defines the root path route ("/")
   # root "articles#index"
